feeae41fa5d154d62dbff4debe1363f985f8f304
znery <218629674+zneryyyy@users.noreply.github.com>
2025-08-18 18:16:15 +0800

    Add region and onecycle modifier for grab pole

--- BingoMode/BingoChallenges/BingoGrabPoleMimicChallenge.cs
+++ BingoMode/BingoChallenges/BingoGrabPoleMimicChallenge.cs
@@ -18,11 +18,15 @@ namespace BingoMode.BingoChallenges
     public class BingoGrabPoleMimicRandomizer : ChallengeRandomizer
     {
         public Randomizer<int> amount;
+        public Randomizer<string> region;
+        public Randomizer<bool> oneCycle;
 
         public override Challenge Random()
         {
             BingoGrabPoleMimicChallenge challenge = new();
             challenge.amount.Value = amount.Random();
+            challenge.region.Value = region.Random();
+            challenge.oneCycle.Value = oneCycle.Random();
             return challenge;
         }
 
@@ -31,6 +35,8 @@ namespace BingoMode.BingoChallenges
             string surindent = indent + INDENT_INCREMENT;
             StringBuilder serializedContent = new();
             serializedContent.AppendLine($"{surindent}amount-{amount.Serialize(surindent)}");
+            serializedContent.AppendLine($"{surindent}region-{region.Serialize(surindent)}");
+            serializedContent.AppendLine($"{surindent}oneCycle-{oneCycle.Serialize(surindent)}");
             return base.Serialize(indent).Replace("__Type__", "GrabPoleMimic").Replace("__Content__", serializedContent.ToString());
         }
 
@@ -38,17 +44,23 @@ namespace BingoMode.BingoChallenges
         {
             Dictionary<string, string> dict = ToDict(serialized);
             amount = Randomizer<int>.InitDeserialize(dict["amount"]);
+            region = Randomizer<string>.InitDeserialize(dict["region"]);
+            oneCycle = Randomizer<bool>.InitDeserialize(dict["oneCycle"]);
         }
     }
 
     public class BingoGrabPoleMimicChallenge : BingoChallenge
     {
         public SettingBox<int> amount;
+        public SettingBox<string> region;
+        public SettingBox<bool> oneCycle;
         public int current;
 
         public BingoGrabPoleMimicChallenge()
         {
             amount = new(0, "Amount", 0);
+            oneCycle = new(false, "In one Cycle", 3);
+            region = new("", "Region", 5, listName: "regions");
         }
 
         public override void UpdateDescription()
@@ -57,19 +69,29 @@ namespace BingoMode.BingoChallenges
             {
                 ChallengeTools.CreatureName(ref ChallengeTools.creatureNames);
             }
-            description = ChallengeTools.IGT.Translate("Grab [<current>/<amount>] Pole Mimics")
+            string location = region.Value != "Any Region" ? Region.GetRegionFullName(region.Value, ExpeditionData.slugcatPlayer) : "";
+            description = ChallengeTools.IGT.Translate("Grab [<current>/<amount>] Pole Mimics <location><onecycle>")
                 .Replace("<current>", ValueConverter.ConvertToString(current))
-                .Replace("<amount>", ValueConverter.ConvertToString(amount.Value));
+                .Replace("<amount>", ValueConverter.ConvertToString(amount.Value))
+                .Replace("<location>", location != "" ? " in " + location : "")
+                .Replace("<onecycle>", oneCycle.Value ? " in one cycle" : "")
+                ;
             base.UpdateDescription();
         }
 
         public override Phrase ConstructPhrase()
         {
-            return new(
-                [
-                [new Icon("steal_item"), Icon.FromEntityName("PoleMimic")],
-                [new Counter(current, amount.Value)]
-                ]);
+            Phrase phrase = new([[new Icon("steal_item"), Icon.FromEntityName("PoleMimic")]]);
+            int lastLine = 1;
+            if (region.Value != "Any Region")
+            {
+                phrase.InsertWord(new Verse(region.Value), 1);
+                lastLine = 2;
+            }
+
+            phrase.InsertWord(new Counter(current, amount.Value), lastLine);
+            if (oneCycle.Value) phrase.InsertWord(new Icon("cycle_limit"), lastLine);
+            return phrase;
         }
 
         public override string ChallengeName()
@@ -84,10 +106,28 @@ namespace BingoMode.BingoChallenges
 
         public override Challenge Generate()
         {
+            bool oneCycle = UnityEngine.Random.value < 0.2f;
+            int amount = UnityEngine.Random.Range(1, oneCycle ? 2 : 5);
             return new BingoGrabPoleMimicChallenge()
             {
-                amount = new(UnityEngine.Random.Range(1, 4), "Amount", 0)
+                amount = new(amount, "Amount", 0),
+                oneCycle = new(oneCycle, "In one Cycle", 3),
+                region = new("Any Region", "Region", 5, listName: "regions"),
             };
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            if (!completed && oneCycle.Value && game != null && game.cameras.Length > 0 && game.cameras[0].room != null && game.cameras[0].room.shelterDoor != null && game.cameras[0].room.shelterDoor.IsClosing)
+            {
+                if (current != 0)
+                {
+                    current = 0;
+                    UpdateDescription();
+                }
+                return;
+            }
         }
 
         public void Grabbed()
@@ -121,6 +161,10 @@ namespace BingoMode.BingoChallenges
                 "><",
                 current.ToString(),
                 "><",
+                region.ToString(),
+                "><",
+                oneCycle.ToString(),
+                "><",
                 completed ? "1" : "0",
                 "><",
                 revealed ? "1" : "0",
@@ -134,6 +178,8 @@ namespace BingoMode.BingoChallenges
                 string[] array = Regex.Split(args, "><");
                 amount = SettingBoxFromString(array[0]) as SettingBox<int>;
                 current = int.Parse(array[1], NumberStyles.Any, CultureInfo.InvariantCulture);
+                region = SettingBoxFromString(array[4]) as SettingBox<string>;
+                oneCycle = SettingBoxFromString(array[5]) as SettingBox<bool>;
                 completed = (array[2] == "1");
                 revealed = (array[3] == "1");
                 UpdateDescription();
@@ -147,7 +193,7 @@ namespace BingoMode.BingoChallenges
 
         public override bool ValidForThisSlugcat(SlugcatStats.Name slugcat)
         {
-            return slugcat.value != "Saint";
+            return true;
         }
 
         public override void AddHooks()
@@ -160,6 +206,6 @@ namespace BingoMode.BingoChallenges
             On.PoleMimic.BeingClimbedOn -= PoleMimic_BeingClimedOn;
         }
 
-        public override List<object> Settings() => [amount];
+        public override List<object> Settings() => [amount, region, oneCycle];
     }
 }
